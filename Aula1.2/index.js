56f75a6a215e82f7ad676a57fd672608adaf1b62
CocoaSweet <li.piva@hotmail.com>
2022-12-27 00:51:42 -0300

    objetos basico

--- Aula1.2/index.js
+++ Aula1.2/index.js
@@ -1,1 +1,43 @@
-console.log('Ola mundo');
+/*const funcionario1 = {
+    nome: 'Kayle', sobrenome: 'Silva', idade: 44
+};
+
+console.log(funcionario1.idade);
+console.log(funcionario1.sobrenome);*/
+
+function criarFuncionario (nome, sobrenome, idade){
+        return{
+            nome: nome, sobrenome: sobrenome, idade: idade
+        };
+};
+
+const funcionario1 = criarFuncionario('Fulano', 'Souza', 88);
+const funcionario2 = criarFuncionario('Beltrana', 'Silva', 34);
+const funcionario3 = criarFuncionario('Sicrano', 'Oliveira', 56);
+
+console.log(funcionario1.nome, funcionario2.sobrenome, funcionario3.idade);
+
+const funcionario4 = {
+    nome: 'Roberta', sobrenome: 'Moreno', idade: 22,
+
+    fala() {
+        console.log(`${this.nome} ${this.sobrenome}, está te dizendo oi...`);
+    },
+
+    falaIdade() {
+        console.log(`${this.nome} ${this.sobrenome} tem ${this.idade} anos.`);
+    },
+
+    incrementaIdade() {
+        this.idade++
+    }
+};
+
+funcionario4.fala();
+funcionario4.falaIdade();
+funcionario4.incrementaIdade();
+funcionario4.falaIdade();
+funcionario4.incrementaIdade();
+funcionario4.falaIdade();
+funcionario4.incrementaIdade();
+funcionario4.falaIdade();
