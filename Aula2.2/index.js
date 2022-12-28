1afc64b3b9f5510a48e32fbdd6ec41390b11be59
CocoaSweet <li.piva@hotmail.com>
2022-12-27 23:06:18 -0300

    valores referenciados ou primitivos

--- Aula2.2/index.js
+++ Aula2.2/index.js
@@ -1,4 +1,33 @@
 let nome = 'Ligia';
 console.log(nome[0]);
 
-let nomes;
+const nomes = ['Ligia', 'Carlos', 'Gui'];
+console.log(nomes[0]);
+
+const num1 = [1, 2, 3];
+const num2 = num1;
+
+console.log(num1, num2);
+
+num1.push(4);
+console.log(num1, num2);
+
+num2.pop();
+console.log(num1, num2);
+
+const num3 = [...num2]; /* valor foi copiado e não referenciado */
+
+num2.push(8);
+console.log(num2, num3);
+
+const a = {
+    nome: 'Maria',
+    sobrenome: 'Silva'
+};
+const b = a;
+const c = {...b};
+
+console.log(b, c);
+
+a.nome = 'Lurdes';
+console.log(b, c);
