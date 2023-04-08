261d0c20bb819bf6d37a98a40e27e4626ab7acda
CocoaSweet <li.piva@hotmail.com>
2023-04-08 00:03:31 -0300

    ex com nodelist

--- Aula33/assets/js/main.js
+++ Aula33/assets/js/main.js
@@ -1,1 +1,12 @@
-console.log(1);
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estiloBody = getComputedStyle(document.body);
+const backgroundColorBody = estiloBody.backgroundColor;
+console.log(backgroundColorBody);
+
+
+for(let p of ps){
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = '#FFFFFF';
+}
